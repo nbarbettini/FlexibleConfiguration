0259cebaef9ab30e0130666972dbc259cd050eb7
Nate Barbettini <nathanaelb@gmail.com>
2016-01-13 16:35:39 -0800

    Add API documentation

--- FlexibleConfiguration/FlexibleConfiguration.cs
+++ FlexibleConfiguration/FlexibleConfiguration.cs
@@ -2,35 +2,53 @@
 // Copyright (c) Nate Barbettini. All rights reserved.
 // </copyright>
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FlexibleConfiguration.Internal;
 using FlexibleConfiguration.Providers;
 
 namespace FlexibleConfiguration
 {
+    /// <summary>
+    /// Builds a configuration object from a variety of sources.
+    /// </summary>
+    /// <typeparam name="T">The .NET type that will store the configuration.</typeparam>
     public class FlexibleConfiguration<T>
         where T : class, new()
     {
         private readonly IConfigurationContext context;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FlexibleConfiguration{T}"/> class.
+        /// </summary>
         public FlexibleConfiguration()
         {
             this.context = new DefaultConfigurationContext();
         }
 
+        /// <summary>
+        /// Adds a configuration value to the current configuration.
+        /// </summary>
+        /// <param name="fullyQualifiedPath">The fully-qualified path, like <c>myValue</c> or <c>foo.bar.myValue</c>.</param>
+        /// <param name="value">The configuration value to store at <paramref name="fullyQualifiedPath"/>.</param>
         public void Add(string fullyQualifiedPath, object value)
             => this.Add(new KeyValuePair<string, object>[] { new KeyValuePair<string, object>(fullyQualifiedPath, value) });
 
+        /// <summary>
+        /// Adds the given configuration values to the current configuration.
+        /// </summary>
+        /// <param name="fullyQualifiedItems">
+        /// Key/value pairs whose key is a fully-qualified path (like <c>myValue</c> or <c>foo.bar.myValue</c>) and whose value is the configuration value.
+        /// </param>
         public void Add(IEnumerable<KeyValuePair<string, object>> fullyQualifiedItems)
         {
             var provider = new ExplicitConfigurationProvider(fullyQualifiedItems);
             provider.ApplyConfiguration(this.context);
         }
 
+        /// <summary>
+        /// Constructs the target configuration object based on the current builder state.
+        /// </summary>
+        /// <returns>An instance of <typeparamref name="T"/>.</returns>
         public T Build()
         {
             var builder = new TargetBuilder(typeof(T), this.context);
--- FlexibleConfiguration/IConfigurationContext.cs
+++ FlexibleConfiguration/IConfigurationContext.cs
@@ -4,10 +4,25 @@
 
 namespace FlexibleConfiguration
 {
+    /// <summary>
+    /// Represents the configuration data for a <see cref="FlexibleConfiguration{T}">configuration builder</see>.
+    /// </summary>
     public interface IConfigurationContext
     {
+        /// <summary>
+        /// Gets the value stored at <paramref name="fullyQualifiedPath"/>.
+        /// </summary>
+        /// <remarks>If the path cannot be fully traversed, <see langword="null"/> is returned.</remarks>
+        /// <param name="fullyQualifiedPath">The fully-qualified path of a configuration value.</param>
+        /// <returns>The configuration value, or <see langword="null"/> if no value exists.</returns>
         object Get(string fullyQualifiedPath);
 
+        /// <summary>
+        /// Stores a configuration value.
+        /// </summary>
+        /// <remarks>If a value already exists at <paramref name="fullyQualifiedPath"/>, it is overwritten.</remarks>
+        /// <param name="fullyQualifiedPath">The fully-qualified path of the configuration value to add.</param>
+        /// <param name="value">The configuration value to store.</param>
         void Put(string fullyQualifiedPath, object value);
     }
 }
