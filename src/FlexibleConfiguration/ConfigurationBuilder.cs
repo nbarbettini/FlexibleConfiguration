fefeb547c4452328fef4dc99d8edd14ab40a299d
Nate Barbettini <nathanaelb@gmail.com>
2016-02-23 11:24:38 -0800

    Migrate code from aspnet/Configuration

---
+++ src/FlexibleConfiguration/ConfigurationBuilder.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Nate Barbettini.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Contains code modified from the ASP.NET Configuration project. Licensed under the Apache License, Version 2.0 from the .NET Foundation.
+
+using System.Collections.Generic;
+using FlexibleConfiguration.Abstractions;
+using FlexibleConfiguration.Internal;
+
+namespace FlexibleConfiguration
+{
+    /// <summary>
+    /// Builds a configuration object from a variety of sources.
+    /// </summary>
+    public sealed class ConfigurationBuilder
+    {
+        private readonly IList<IConfigurationProvider> providers = new List<IConfigurationProvider>();
+
+        /// <summary>
+        /// Adds a new configuration provider.
+        /// </summary>
+        /// <param name="provider">The configuration provider to add.</param>
+        /// <returns>The same <see cref="IConfigurationBuilder"/>.</returns>
+        public ConfigurationBuilder Add(IConfigurationProvider provider)
+        {
+            provider.Load();
+            this.providers.Add(provider);
+            return this;
+        }
+
+        /// <summary>
+        /// Constructs the target configuration object based on the current builder state.
+        /// </summary>
+        /// <returns>An instance of <typeparamref name="T"/>.</returns>
+        public IConfigurationRoot Build()
+        {
+            return new ConfigurationRoot(this.providers);
+        }
+    }
+}
---
+++ src/FlexibleConfiguration/Internal/ConfigurationRoot.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Nate Barbettini.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Contains code modified from the ASP.NET Configuration project. Licensed under the Apache License, Version 2.0 from the .NET Foundation.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FlexibleConfiguration.Abstractions;
+
+namespace FlexibleConfiguration.Internal
+{
+    public class ConfigurationRoot : IConfigurationRoot
+    {
+        private IList<IConfigurationProvider> _providers;
+
+        public ConfigurationRoot(IList<IConfigurationProvider> providers)
+        {
+            if (providers == null)
+            {
+                throw new ArgumentNullException(nameof(providers));
+            }
+
+            _providers = providers;
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                foreach (var provider in _providers.Reverse())
+                {
+                    string value;
+
+                    if (provider.TryGet(key, out value))
+                    {
+                        return value;
+                    }
+                }
+
+                return null;
+            }
+
+            set
+            {
+                if (!_providers.Any())
+                {
+                    throw new InvalidOperationException("No providers defined.");
+                }
+
+                foreach (var provider in _providers)
+                {
+                    provider.Set(key, value);
+                }
+            }
+        }
+
+        public IEnumerable<IConfigurationSection> GetChildren() => GetChildrenImplementation(null);
+
+        internal IEnumerable<IConfigurationSection> GetChildrenImplementation(string path)
+        {
+            return _providers
+                .Aggregate(Enumerable.Empty<string>(),
+                    (seed, source) => source.GetChildKeys(seed, path))
+                .Distinct()
+                .Select(key => GetSection(path == null ? key : ConfigurationPath.Combine(path, key)));
+        }
+
+        public IConfigurationSection GetSection(string key)
+        {
+            return new ConfigurationSection(this, key);
+        }
+    }
+}
