3968dcbdca24e7884aa489fba6d1ae4d3878621f
Nate Barbettini <nathanaelb@gmail.com>
2016-02-22 17:09:13 -0800

    Bump version to 0.2

--- FlexibleConfiguration/Properties/AssemblyInfo.cs
+++ FlexibleConfiguration/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("93d8cba6-cf26-4944-8292-6e35392b572d")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
 [assembly: InternalsVisibleTo("FlexibleConfiguration.Tests")]
