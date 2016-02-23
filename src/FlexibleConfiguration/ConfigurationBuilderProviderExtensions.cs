0fec558dd8625237497cfcbd2923b3585c44f531
Nate Barbettini <nathanaelb@gmail.com>
2016-02-23 13:38:09 -0800

    Add in-memory provider

--- src/FlexibleConfiguration/ConfigurationBuilderProviderExtensions.cs
+++ src/FlexibleConfiguration/ConfigurationBuilderProviderExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Nate Barbettini.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
 using FlexibleConfiguration.Providers;
 
 namespace FlexibleConfiguration
@@ -163,6 +165,25 @@ namespace FlexibleConfiguration
         {
             var contents = FileOperations.Load(filePath, required);
             return builder.AddProperties(contents);
+        }
+
+        /// <summary>
+        /// Adds the memory configuration provider to <paramref name="configurationBuilder"/>.
+        /// </summary>
+        /// <param name="configurationBuilder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+        /// <param name="initialData">The data to add to memory configuration provider.</param>
+        /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        public static ConfigurationBuilder AddInMemoryCollection(
+            this ConfigurationBuilder configurationBuilder,
+            IEnumerable<KeyValuePair<string, string>> initialData)
+        {
+            if (configurationBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(configurationBuilder));
+            }
+
+            configurationBuilder.Add(new MemoryConfigurationProvider(initialData));
+            return configurationBuilder;
         }
     }
 }
