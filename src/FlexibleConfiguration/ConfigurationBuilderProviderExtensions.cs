b6e6d3c82f32b2bb89d65007b81851bff3a75814
Nate Barbettini <nathanaelb@gmail.com>
2016-06-10 18:50:44 -0700

    Add logging and clean up doc comments

--- src/FlexibleConfiguration/ConfigurationBuilderProviderExtensions.cs
+++ src/FlexibleConfiguration/ConfigurationBuilderProviderExtensions.cs
@@ -17,12 +17,14 @@ namespace FlexibleConfiguration
         /// <param name="builder">The <see cref="IIConfigurationBuilder"/> to add to.</param>
         /// <param name="mustStartWith">The prefix that environment variable names must start with.</param>
         /// <param name="separator">The separator character or string between key and value names.</param>
-        /// <returns>The <see cref="IIConfigurationBuilder"/>.</returns>
+        /// <param name="logger">The logger.</param>
+        /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
         public static IConfigurationBuilder AddEnvironmentVariables(
             this IConfigurationBuilder builder,
             string mustStartWith,
             string separator,
-            string root)
+            string root,
+            ILogger logger = null)
         {
             builder.Add(new EnvironmentVariablesProvider(mustStartWith, separator, root));
             return builder;
@@ -38,12 +40,14 @@ namespace FlexibleConfiguration
         /// For example, if <paramref name="root"/> is <c>foo</c>, and the value <c>bar = baz</c>
         /// is discovered, the actual added value will be <c>foo.bar = baz</c>.
         /// </param>
+        /// <param name="logger">The logger.</param>
         public static IConfigurationBuilder AddJson(
             this IConfigurationBuilder builder,
             string json,
-            string root = null)
+            string root = null,
+            ILogger logger = null)
         {
-            var provider = new JsonProvider(json, root);
+            var provider = new YamlProvider(json, root, logger);
             return builder.Add(provider);
         }
 
@@ -52,21 +56,23 @@ namespace FlexibleConfiguration
         /// </summary>
         /// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
         /// <param name="filePath">The file path.</param>
-        /// <param name="required">
-        /// Determines whether the file can be skipped silently. If <paramref name="required"/> is <see langword="true"/>,
-        /// and the file does not exist, a <see cref="System.IO.FileNotFoundException"/> will be thrown. If <paramref name="required"/>
-        /// is <see langword="false"/>, the method will return silently.
+        /// <param name="optional">
+        /// Determines whether the file can be skipped silently. If <paramref name="optional"/> is <see langword="false"/>,
+        /// and the file does not exist, a <see cref="System.IO.FileNotFoundException"/> will be thrown. If <paramref name="optional"/>
+        /// is <see langword="true"/>, the method will return silently.
         /// </param>
         /// <param name="root">
         /// An optional root name to apply to any configuration values.
         /// For example, if <paramref name="root"/> is <c>foo</c>, and the value <c>bar = baz</c>
         /// is discovered, the actual added value will be <c>foo.bar = baz</c>.
         /// </param>
+        /// <param name="logger">The logger.</param>
         public static IConfigurationBuilder AddJsonFile(
             this IConfigurationBuilder builder,
             string filePath,
             bool optional = false,
-            string root = null)
+            string root = null,
+            ILogger logger = null)
         {
             var json = FileOperations.Load(filePath, optional);
             return builder.AddJson(json, root);
@@ -82,12 +88,14 @@ namespace FlexibleConfiguration
         /// For example, if <paramref name="root"/> is <c>foo</c>, and the value <c>bar = baz</c>
         /// is discovered, the actual added value will be <c>foo.bar = baz</c>.
         /// </param>
+        /// <param name="logger">The logger.</param>
         public static IConfigurationBuilder AddYaml(
             this IConfigurationBuilder builder,
             string yaml,
-            string root = null)
+            string root = null,
+            ILogger logger = null)
         {
-            var provider = new YamlProvider(yaml, root);
+            var provider = new YamlProvider(yaml, root, logger);
             return builder.Add(provider);
         }
 
@@ -106,11 +114,13 @@ namespace FlexibleConfiguration
         /// For example, if <paramref name="root"/> is <c>foo</c>, and the value <c>bar = baz</c>
         /// is discovered, the actual added value will be <c>foo.bar = baz</c>.
         /// </param>
+        /// <param name="logger">The logger.</param>
         public static IConfigurationBuilder AddYamlFile(
             this IConfigurationBuilder builder,
             string filePath,
             bool optional = false,
-            string root = null)
+            string root = null,
+            ILogger logger = null)
         {
             var yaml = FileOperations.Load(filePath, optional);
             return builder.AddYaml(yaml, root);
@@ -125,10 +135,12 @@ namespace FlexibleConfiguration
         /// </remarks>
         /// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
         /// <param name="multiLineConfiguration">The configuration string.</param>
+        /// <param name="logger">The logger.</param>
         public static IConfigurationBuilder AddProperties(
             this IConfigurationBuilder builder,
             string multiLineConfiguration,
-            string root = null)
+            string root = null,
+            ILogger logger = null)
         {
             var provider = new PropertiesFileProvider(multiLineConfiguration, root);
             return builder.Add(provider);
@@ -153,11 +165,13 @@ namespace FlexibleConfiguration
         /// For example, if <paramref name="root"/> is <c>foo</c>, and the value <c>bar = baz</c>
         /// is discovered, the actual added value will be <c>foo.bar = baz</c>.
         /// </param>
+        /// <param name="logger">The logger.</param>
         public static IConfigurationBuilder AddPropertiesFile(
             this IConfigurationBuilder builder,
             string filePath,
             bool optional = false,
-            string root = null)
+            string root = null,
+            ILogger logger = null)
         {
             var contents = FileOperations.Load(filePath, optional);
             return builder.AddProperties(contents, root);
@@ -166,12 +180,14 @@ namespace FlexibleConfiguration
         /// <summary>
         /// Adds the memory configuration provider to <paramref name="IConfigurationBuilder"/>.
         /// </summary>
-        /// <param name="IConfigurationBuilder">The <see cref="IIConfigurationBuilder"/> to add to.</param>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
         /// <param name="initialData">The data to add to memory configuration provider.</param>
-        /// <returns>The <see cref="IIConfigurationBuilder"/>.</returns>
+        /// <param name="logger">The logger.</param>
+        /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
         public static IConfigurationBuilder AddInMemoryCollection(
             this IConfigurationBuilder builder,
-            IEnumerable<KeyValuePair<string, string>> initialData)
+            IEnumerable<KeyValuePair<string, string>> initialData,
+            ILogger logger = null)
         {
             if (builder == null)
             {
@@ -182,16 +198,18 @@ namespace FlexibleConfiguration
         }
 
         /// <summary>
-        /// Adds the configuration in <paramref name="object"/> to <paramref name="IConfigurationBuilder"/>.
+        /// Adds the configuration in <paramref name="object"/> to the <see cref="IConfigurationBuilder"/>.
         /// </summary>
-        /// <param name="builder">The <see cref="IIConfigurationBuilder"/> to add to.</param>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
         /// <param name="object">The object to examine.</param>
         /// <param name="root">A root element to prepend to any discovered key.</param>
+        /// <param name="logger">The logger.</param>
         /// <returns>The <see cref="builder"/>.</returns>
         public static IConfigurationBuilder AddObject(
             this IConfigurationBuilder builder,
             object @object,
-            string root = null)
+            string root = null,
+            ILogger logger = null)
         {
             if (builder == null)
             {
